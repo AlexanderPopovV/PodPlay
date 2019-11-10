6126d2e14948405ae262bd4bf8828d791f99702c
Alexander Popov <PopovAlexanderVyacheslavovich@gmail.com>
2019-11-10 22:09:44 +0300

    Initial commit

---
+++ app/src/main/java/com/alexpop/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,13 @@
+package com.alexpop.podplay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexpop.podplay.R
+
+class PodcastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
